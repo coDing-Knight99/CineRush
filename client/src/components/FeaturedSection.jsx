05350dd2fed3bb10acfe7f2c993daa8147a38764
coDing-Knight99 <dhruvkanojiawork@gmail.com>
2025-07-11 22:53:31 +0530

    Home page Complete

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -9,7 +9,9 @@ const FeaturedSection = () => {
     return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
         <div className='relative flex items-center justify-between pt-20 pb-10'>
+
             <BlurCircle top='0' right='-80px' />
+            <BlurCircle  left='-100px' bottom='-100px'/>
             <p>Now Showing</p>
             <button onClick={()=>navigate('/movies')} className='group flex items-center gap-2 teext-sm text-gray-300 cursor-pointer'>
                 View All 
@@ -18,13 +20,13 @@ const FeaturedSection = () => {
         </div>
       
       <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
-      {dummyShowsData.slice(0,6).map((show)=>(
+      {dummyShowsData.slice(0,5).map((show)=>(
         <MovieCard key={show._id} movie={show}/>
       ))}
     </div>
 
-      <div className='flex justify-center mt-20'>
-        <button onClick={()=>{navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show More</button>
+      <div className='flex justify-center m-20 '>
+        <button onClick={()=>{navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Show More</button>
       </div>
     </div>
   )
